431b4df844028e0386603c97314e82f34fef4597
azaa1 <azaa.chi11@gmail.com>
2018-12-08 19:20:49 -0600

    changed app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            This is a test5 app!
+            This is a feature branch test!
           </p>
           <a
             className="App-link"
