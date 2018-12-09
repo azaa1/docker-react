6544e36a3f619fb29e490f5547e9c7b5f8dda730
azaa1 <azaa.chi11@gmail.com>
2018-12-08 18:55:08 -0600

    Source Code Change

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            This is a test4 app!
+            This is a test5 app!
           </p>
           <a
             className="App-link"
